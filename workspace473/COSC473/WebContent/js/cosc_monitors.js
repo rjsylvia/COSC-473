214cfc7ff06be960d735184261cb123a188addb3
dkonawalik <daniel.konawalik@gmail.com>
2016-03-08 02:44:31 -0500

    (March 8th) Added more cycling animations, organization of CSS/JS + code comments
    Cycling animations now display for the following (currently all switch simultaneously at 30 seconds, will be increased eventually):
    1) Social media section headers (Changes between "IUP Twitter" and "IUP Facebook", set to sync with their respective widgets)
    2) Class schedule and (added) COSC Events/Tutoring headers with data sections (currently COSC Events/Tutoring shows nothing inside)
    3) IUP Events renamed to "IUP Campus Events" and now cycles with (added) IUP Sports News (headers only currently)
    4) National/Worldwide news header renamed to just "National News" (for possible future cycling between a worldwide news livestream)
    
    Also included:
    - Removed one more CSS reference inside cosc_monitors.css that referred to the mockup project
    - Panels now classified as "leftside" or "rightside" panels and no longer use individual ids to help clean up repetitive CSS code
    - cosc_monitors.css file now contains commented sections

--- workspace473/COSC473/WebContent/js/cosc_monitors.js
+++ workspace473/COSC473/WebContent/js/cosc_monitors.js
@@ -2,12 +2,59 @@
  * Main JavaScript file for the COSC Hall Monitors
  */
 
+$(document).ready(function(){
+    datetime = $('#datetime');			// Assign the scrolling date to the html id
+	day = $('#day');					// Assign the day to the html day id used in Class Schedule header
+	getDayAbbrev();						// Get the current day abbreviation
+    updateDate();						// Continuously update the datetime shown to keep in sync with time on machine
+    setInterval(updateDate, 1000); 			// Update every second to project real time
+	
+	getWeather();              			// Gets the initial weather on load of page
+	setInterval(getWeather, 180000); 	// Update every 3 min
+	 
+	getClasses();						// Get the class schedule from the appropriate CSV file
+	setInterval(getClasses, 10800000); 	// Update every 3 hours to stay updated for next day
+	
+	setInterval(reloadNewsLiveFeed, 10800000); // Reload every 3 hours to refresh the live feed
+	
+	// Cycles transition between headers/info every 29 seconds (additional 1 second for transition)
+	// totaling 30 seconds total. This applies to all of the cycle function calls below.
+	$('.social_media').cycle({
+	    speed: 1000,
+	    timeout: 29000
+	});
+	
+	$('#media_headers').cycle({
+	    speed: 1000,
+	    timeout: 29000
+	});
+	
+	$('.comp_sci_info').cycle({
+	    speed: 1000,
+	    timeout: 29000
+	});
+	
+	$('#cosc_headers').cycle({
+	    speed: 1000,
+	    timeout: 29000
+	});
+	
+	$('#iup_headers').cycle({
+	    speed: 1000,
+	    timeout: 29000
+	});
+});
+
+// Declare some variables to be used in functions
 var datetime = null,
         date = null;
 		day = null;
 		schedule = null;
 		abbrev = null;
 
+/*
+ * Gets the current day abbreviation
+ */
 var getDayAbbrev = function () {
 	switch (new Date().getDay()) {
     case 0:
@@ -34,39 +81,25 @@ var getDayAbbrev = function () {
 	}
 }
 		
-var update = function () {
+/*
+ * Update the datetime to keep in sync with time on machine
+ */
+var updateDate = function () {
     date = moment(new Date())
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 
 	day.html(date.format('dddd'));
 };
 
-$(document).ready(function(){
-    datetime = $('#datetime');
-	day = $('#day');
-	getDayAbbrev();
-    update();
-    setInterval(update, 1000); 			// Update every second to project real time
-	
-	getWeather();              			// Gets the initial weather on load of page
-	setInterval(getWeather, 180000); 	// Update every 3 min
-	 
-	getClasses();
-	setInterval(getClasses, 10800000); 	// Update every 3 hours to stay updated for next day
-	
-	setInterval(reloadNewsLiveFeed, 10800000); // Reload every 3 hours to refresh the live feed
-	
-	$('.social_media').cycle({
-	    speed: 1000,
-	    timeout: 29000
-	});
-});
-
+/*
+ * Get the class schedule for the appropriate room reading the appropriate CSV file
+ * to display the information
+ */
 function getClasses() {
 	// This will need changing to dynamically pick what file to read depending on monitor
 	
 	$.get('data/str112A_data.csv', function(data) {
-		var build = '<table cellpadding="2" cellspacing="2" style="border-collapse: collapse" width="100%">\n';
+		var build = '<table cellpadding="2" cellspacing="2">\n';
 		var rows = data.split("\n");
 		rows.forEach( function getvalues(thisRow) {
 			build += "<tr>\n";
@@ -88,6 +121,9 @@ function getClasses() {
 	});
 }
 
+/*
+ * Get the weather information to display in the top right
+ */
 function getWeather() {
 	$.simpleWeather({
     location: 'Indiana, PA',
@@ -104,6 +140,9 @@ function getWeather() {
   });
 }
 
+/*
+ * Reload the live news feed to make sure it is consistently running
+ */
 function reloadNewsLiveFeed() {
 	$("#livefeed").contentWindow.location.reload();
 }
